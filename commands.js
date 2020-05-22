0e3a61eb35f243a6b1dfc7e58e3f6c18455e38dc
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-22 15:55:22 +1000

    err yea

--- commands.js
+++ commands.js
@@ -11,6 +11,35 @@ const TimedCache = require('./TimedCache')
 
 let cache = new TimedCache(1000 * 60)
 
+let messages = [
+  "%s will no longer be a problem",
+  "*shoots %s*. **HEADSHOT!**",
+  "Unfortunately, %s killed me instead",
+  "%s has been disposed of.",
+  "%s has been crushed by a piano",
+  "A pack of wild wolves ate %s",
+  "%s committed suicide instead of git commit.",
+  "%s? They were trampled to death at a football match",
+  "Why did you kill %s? Who is going to clean it up?",
+  "Too little MLP watching killed %s",
+  "Using Internet Explorer killed %s",
+  "%s stepped on a landmine",
+  "%s has been given a lethal injection.",
+  "%s? Their computer blew up",
+  "%s didn't watch their step and fell off a cliff",
+  "%s traveled back in time and killed themselves",
+  "*Sending poisoned dinner to %s...*",
+  "%s spontaneously combusted.",
+  "%s? They went into the forest and never came back",
+  "%s fell in front of a train",
+  "%s was pushed off a bridge",
+  "%s was involved in an airplane accident",
+  "Ninjas surrounded %s one day... That is all.",
+  "Poor %s. They fell into an endless pit",
+  "Mutants killed %s",
+  "%s died from licking something they shouldn't have licked"
+]
+
 module.exports = {
   help: {
     exec: msg => {
@@ -59,7 +88,10 @@ module.exports = {
     exec: msg => {
       let user = msg.mentions.users.first()
       if (!user) return
-      msg.reply(`Pew pew! ${user} died!`)
+
+      let message = messages[Math.round(Math.random() * (messages.length - 1))]
+
+      msg.reply(message.replace(/%s/g, user))
     }
   },
   motd: {
