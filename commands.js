963ca896bfc30cd95de4a10bf7b8e4bfe4a2a268
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-25 01:00:27 +1000

    flip

--- commands.js
+++ commands.js
@@ -40,22 +40,86 @@ let messages = [
   "%s died from licking something they shouldn't have licked"
 ]
 
+let flipTable = {
+  '\u0021' : '\u00A1',
+  '\u0022' : '\u201E',
+  '\u0026' : '\u214B',
+  '\u0027' : '\u002C',
+  '\u0028' : '\u0029',
+  '\u002E' : '\u02D9',
+  '\u0033' : '\u0190',
+  '\u0034' : '\u152D',
+  '\u0036' : '\u0039',
+  '\u0037' : '\u2C62',
+  '\u003B' : '\u061B',
+  '\u003C' : '\u003E',
+  '\u003F' : '\u00BF',
+  '\u0041' : '\u2200',
+  '\u0042' : '𐐒',
+  '\u0043' : '\u2183',
+  '\u0044' : '\u25D6',
+  '\u0045' : '\u018E',
+  '\u0046' : '\u2132',
+  '\u0047' : '\u2141',
+  '\u004A' : '\u017F',
+  '\u004B' : '\u22CA',
+  '\u004C' : '\u2142',
+  '\u004D' : '\u0057',
+  '\u004E' : '\u1D0E',
+  '\u0050' : '\u0500',
+  '\u0051' : '\u038C',
+  '\u0052' : '\u1D1A',
+  '\u0054' : '\u22A5',
+  '\u0055' : '\u2229',
+  '\u0056' : '\u1D27',
+  '\u0059' : '\u2144',
+  '\u005B' : '\u005D',
+  '\u005F' : '\u203E',
+  '\u0061' : '\u0250',
+  '\u0062' : '\u0071',
+  '\u0063' : '\u0254',
+  '\u0064' : '\u0070',
+  '\u0065' : '\u01DD',
+  '\u0066' : '\u025F',
+  '\u0067' : '\u0183',
+  '\u0068' : '\u0265',
+  '\u0069' : '\u0131',
+  '\u006A' : '\u027E',
+  '\u006B' : '\u029E',
+  '\u006C' : '\u0283',
+  '\u006D' : '\u026F',
+  '\u006E' : '\u0075',
+  '\u0072' : '\u0279',
+  '\u0074' : '\u0287',
+  '\u0076' : '\u028C',
+  '\u0077' : '\u028D',
+  '\u0079' : '\u028E',
+  '\u007B' : '\u007D',
+  '\u203F' : '\u2040',
+  '\u2045' : '\u2046',
+  '\u2234' : '\u2235'
+}
+
+for (let i in flipTable) {
+  flipTable[flipTable[i]] = i
+}
+
 module.exports = {
   help: {
     exec: msg => {
       msg.reply(`
-!help: lists the commands
-!hello: say hi
-!is_ron_straight: is ron straight?
-!straight?: is the person straight?
-!kill: kill someone
-!motd: message
-!avatar: show a user's avatar
-!waifu <count>: sends waifu
-!pronouns: what pronouns does the bot use
-!track: tracks a bus
-!classes: what is my next class
-`)
+        !help: lists the commands
+        !hello: say hi
+        !is_ron_straight: is ron straight?
+        !straight?: is the person straight?
+        !kill: kill someone
+        !motd: message
+        !avatar: show a user's avatar
+        !waifu <count>: sends waifu
+        !pronouns: what pronouns does the bot use
+        !track: tracks a bus
+        !classes: what is my next class
+      `)
     }
   },
   hello: {
@@ -79,9 +143,9 @@ module.exports = {
       let straight = ['Xager_0', 'Incompetent']
 
       if (straight.includes(username))
-        msg.reply(`Yes, ${user} is straight`)
+      msg.reply(`Yes, ${user} is straight`)
       else
-        msg.reply(`No, ${user} is not straight`)
+      msg.reply(`No, ${user} is not straight`)
     }
   },
   kill: {
@@ -129,8 +193,8 @@ module.exports = {
       if (!fleet) return msg.reply('You need to specify a fleet #')
 
       let data = JSON.parse(await request(`https://vic.transportsg.me/tracker2/bus-bot?fleet=${fleet}`))
-      let text = data.map(trip => {
-        return `${trip.departureTime} ${trip.routeNumber}: ${trip.origin} - ${trip.destination}`
+        let text = data.map(trip => {
+          return `${trip.departureTime} ${trip.routeNumber}: ${trip.origin} - ${trip.destination}`
       }).join('\n')
 
       msg.reply(`Trips today: \n${text}`)
@@ -142,71 +206,71 @@ module.exports = {
       if (!service) return msg.reply('You need to specify a service #')
 
       let data = JSON.parse(await request(`https://vic.transportsg.me/tracker2/bus-bot?service=${service}`))
-      let text = data.map(trip => {
-        return `${trip.fleetNumber}: ${trip.departureTime} ${trip.origin} - ${trip.destination}`
-      }).join('\n')
+        let text = data.map(trip => {
+          return `${trip.fleetNumber}: ${trip.departureTime} ${trip.origin} - ${trip.destination}`
+        }).join('\n')
 
-      msg.reply(`Trips today: \n${text}`)
-    }
-  },
-  classes: {
-    exec: async (msg, args) => {
-      let target = msg.mentions.users.first() || msg.author
-      let user = `${target.username}#${target.discriminator}`
-
-      let matchingClasses
-      if (args[0] && args[0].length === 4) {
-        matchingClasses = classes.filter(clazz => clazz.teacher === args[0])
-      } else if (config.CLASSES[user]) {
-        matchingClasses = classes.filter(clazz => clazz.students.includes(config.CLASSES[user]))
+        msg.reply(`Trips today: \n${text}`)
       }
+    },
+    classes: {
+      exec: async (msg, args) => {
+        let target = msg.mentions.users.first() || msg.author
+        let user = `${target.username}#${target.discriminator}`
 
-      if (matchingClasses) {
-        matchingClasses = matchingClasses.map(clazz => {
-          let classCode = clazz.classCode
-          let subjectCode = classCode.replace(/\d[A-Z]?$/, '')
-          let subjectName = codeToNames[subjectCode]
-
-          let start = moment.tz(clazz.start, 'Australia/Melbourne')
-          let end = moment.tz(clazz.end, 'Australia/Melbourne')
-
-          let activityID = activities[clazz.classCode]
-          let startTimestamp = moment.utc(clazz.start).format('DDMMYYYYHHmm')
-
-          let compassURL = `https://jmss-vic.compass.education/Organise/Activities/Activity.aspx#session/${activityID}${startTimestamp}`
-
-          return {
-            classCode,
-            subjectName,
-            location: clazz.location,
-            teacher: clazz.teacher,
-            start: start.format('HH:mm'),
-            end: end.format('HH:mm'),
-            startTime: start,
-            endTime: end,
-            compassURL
-          }
-        })
-
-        let now = moment.tz('Australia/Melbourne')
-
-        let upcoming = matchingClasses.filter(event => event.endTime > now).sort((a, b) => a.startTime - b.startTime)
-
-        let current, next, following
-
-        if (upcoming[0].startTime < now) {
-          current = upcoming[0]
-          next = upcoming[1]
-          following = upcoming[2]
-        } else {
-          next = upcoming[0]
-          following = upcoming[1]
+        let matchingClasses
+        if (args[0] && args[0].length === 4) {
+          matchingClasses = classes.filter(clazz => clazz.teacher === args[0])
+        } else if (config.CLASSES[user]) {
+          matchingClasses = classes.filter(clazz => clazz.students.includes(config.CLASSES[user]))
         }
 
-        let embeds = []
+        if (matchingClasses) {
+          matchingClasses = matchingClasses.map(clazz => {
+            let classCode = clazz.classCode
+            let subjectCode = classCode.replace(/\d[A-Z]?$/, '')
+            let subjectName = codeToNames[subjectCode]
 
-        function createEmbed(type, clazz) {
-          embeds.push(new MessageEmbed()
+            let start = moment.tz(clazz.start, 'Australia/Melbourne')
+            let end = moment.tz(clazz.end, 'Australia/Melbourne')
+
+            let activityID = activities[clazz.classCode]
+            let startTimestamp = moment.utc(clazz.start).format('DDMMYYYYHHmm')
+
+            let compassURL = `https://jmss-vic.compass.education/Organise/Activities/Activity.aspx#session/${activityID}${startTimestamp}`
+
+            return {
+              classCode,
+              subjectName,
+              location: clazz.location,
+              teacher: clazz.teacher,
+              start: start.format('HH:mm'),
+              end: end.format('HH:mm'),
+              startTime: start,
+              endTime: end,
+              compassURL
+            }
+          })
+
+          let now = moment.tz('Australia/Melbourne')
+
+          let upcoming = matchingClasses.filter(event => event.endTime > now).sort((a, b) => a.startTime - b.startTime)
+
+          let current, next, following
+
+          if (upcoming[0].startTime < now) {
+            current = upcoming[0]
+            next = upcoming[1]
+            following = upcoming[2]
+          } else {
+            next = upcoming[0]
+            following = upcoming[1]
+          }
+
+          let embeds = []
+
+          function createEmbed(type, clazz) {
+            embeds.push(new MessageEmbed()
             .setTitle(`${type}: ${clazz.subjectName}`)
             .setURL(clazz.compassURL)
             .addFields(
@@ -263,6 +327,19 @@ module.exports = {
       setTimeout(() => {
         setClassStatus(true)
       }, (args[0] * 1000 * 60) || 60000)
+    }
+  },
+  flip: {
+    exec: (msg, args) => {
+      let input = args.join(' ')
+      var last = input.length - 1
+      var result = new Array(input.length)
+      for (var i = last; i >= 0; --i) {
+        var c = input.charAt(i)
+        var r = flipTable[c]
+        result[last - i] = r != undefined ? r : c
+      }
+      msg.reply(result.join(''))
     }
   }
 }
